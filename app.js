dadad5ce08308b689c72437573898f37f0114c55
Alexkings47 <michealexcel54@gmail.com>
2022-06-24 16:19:58 +0100

    created calculato

---
+++ app.js
@@ -0,0 +1,38 @@
+``
+console.log("Calculator. Add, Subtract, Divide, Multiply, e.g calculator(add,9,5,8)")
+
+function calculator(a,b,...numbers){
+  let operator = a.toLowerCase();
+  console.log(operator)
+  let total = b;
+
+  switch (operator){
+    case "add":
+          for (number of numbers){
+              total +=number
+          }
+          break;
+    case "divide":
+          for (number of numbers){
+              total /=number
+          }
+          break;
+    case "multiply":
+          for (number of numbers){
+              total *=number
+          }
+          break;
+    case "subtract":
+          for (number of numbers){
+              total -=number
+          }
+          break;
+         
+  }
+ return total;
+  
+}
+
+
+
+console.log(calculator("add",4,5,6))
